aa48fce6f4377237a91a067cdfbd16bb585ad2f9
Josh Ajiniran <josuajiniran@gmail.com>
2021-10-05 15:11:50 +0100

    add celery mock tests

--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,3 +1,25 @@
+from worker import create_task
+from unittest.mock import patch, call
+
+
 def test_home(test_app):
     response = test_app.get("/")
     assert response.status_code == 200
+
+
+def test_task():
+    assert create_task.run(1)
+    assert create_task.run(2)
+    assert create_task.run(3)
+
+
+@patch("worker.create_task.run")
+def test_mock_task(mock_run):
+    assert create_task.run(1)
+    create_task.run.assert_called_once_with(1)
+
+    assert create_task.run(2)
+    assert create_task.run.call_count == 2
+
+    assert create_task.run(3)
+    assert create_task.run.call_count == 3
